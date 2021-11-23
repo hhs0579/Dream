8136338ebc0b3c8a2c34f417d3beba56370b026e
hhs0579 <hhs0579@naver.com>
2021-11-23 19:29:54 +0900

    hi

--- dreamproject/lib/main.dart
+++ dreamproject/lib/main.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
+import 'login_page.dart';
+import 'home_page.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  final routes = <String, WidgetBuilder>{
+    LoginPage.tag: (context) => LoginPage(),
+    HomePage.tag: (context) => HomePage(),
+  };
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return MaterialApp(
+      title: 'Kodeversitas',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue,
+        fontFamily: 'Nunito',
+      ),
+      home: LoginPage(),
+      routes: routes,
     );
   }
 }
