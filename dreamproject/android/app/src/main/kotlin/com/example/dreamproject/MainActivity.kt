d28b7a82e772b639f5d721759bab6c7581b6a261
hhs0579 <hhs0579@naver.com>
2021-11-23 17:35:35 +0900

    first

---
+++ dreamproject/android/app/src/main/kotlin/com/example/dreamproject/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dreamproject
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ dreamproject/lib/states/user_provider.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+class UserProvider extends ChangeNotifier {
+  bool _userLoggedIn = true;
+
+  void setUserAuth(bool authState) {
+    _userLoggedIn = authState;
+    notifyListeners();
+  }
+
+  bool get userState => _userLoggedIn;
+
+}
