5529a09f817d6684d2e64fa1ca2b6edbd7e8c157
hhs0579 <hhs0579@naver.com>
2021-11-24 10:08:03 +0900

    feed-drawer

--- dreamproject/lib/screens/pages/feed.dart
+++ dreamproject/lib/screens/pages/feed.dart
@@ -1,33 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:simple_drawer/simple_drawer.dart';
 
-class Feed extends StatelessWidget {
-  const Feed({ Key? key }) : super(key: key);
+class Feed extends StatefulWidget {
+  Feed({Key? key}) : super(key: key);
+
+  @override
+  State<Feed> createState() => _FeedState();
+}
+
+class _FeedState extends State<Feed> {
+  GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       
+      endDrawer: Container(
+        width: MediaQuery.of(context).size.width / 2,
+         child: Drawer(),
+      ),
+
       appBar: AppBar(
-        title:Text(
-            'FEED',
-            style:TextStyle(color:Colors.black),
-          ),
-          centerTitle: true,
+        title: Text(
+          'FEED',
+          style: TextStyle(color: Colors.black),
+        ),
+        centerTitle: true,
         backgroundColor: Colors.transparent,
         elevation: 0.0,
-        leading:IconButton(
-          icon:Icon(Icons.receipt_long,color:Colors.blue[300]),
-          onPressed: (){
-          },
-        ),
-        actions:<Widget> [
+        leading: IconButton(
+            icon: Icon(Icons.receipt_long, color: Colors.blue[300]),
+            onPressed: () {}),
+        actions: <Widget>[
           IconButton(
-            icon:Icon(Icons.more_vert,color:Colors.blue[300]),
-            onPressed: ()=> Scaffold.of(context).openDrawer( 
-
-            )
-          )
+              icon: Icon(Icons.more_vert, color: Colors.blue[300]),
+              onPressed: () => _scaffoldKey.currentState!.openEndDrawer(
+              )),
         ],
       ),
     );
