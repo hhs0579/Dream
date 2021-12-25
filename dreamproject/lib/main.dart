2088be8c7f155831765a160a3b93b7a2e6714b2d
hhs0579 <hhs0579@naver.com>
2021-12-25 15:24:32 +0900

    fire

--- dreamproject/lib/main.dart
+++ dreamproject/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:dreamproject/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'controller/homepage_controller.dart';
@@ -6,12 +7,22 @@ import 'screens/pages/feed.dart';
 import 'screens/starts/login_page.dart';
 import 'screens/starts/join_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+class _MyAppState extends State<MyApp> {
   final routes = <String, WidgetBuilder>{};
 
   @override
